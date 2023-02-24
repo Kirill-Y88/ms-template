25b7155ea06bdc3a27a6a3ccc102e9592204010f
KirillPushkin <benberov09@mail.ru>
2023-02-24 18:53:33 +0300

    add hystrix client

---
+++ ms-servise-hystrix/src/main/java/y88/kirill/msservisehystrix/MsServiseHystrixApplication.java
@@ -0,0 +1,13 @@
+package y88.kirill.msservisehystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServiseHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsServiseHystrixApplication.class, args);
+    }
+
+}
---
+++ ms-servise-hystrix/src/test/java/y88/kirill/msservisehystrix/MsServiseHystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package y88.kirill.msservisehystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiseHystrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
